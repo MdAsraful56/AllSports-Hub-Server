de5f673e82f1d5b343a40acfebdb94556648486b
MdAsraful56 <work.mdasraful56@gmail.com>
2025-03-19 06:53:09 +0600

    all sports hub server

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000
+
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+    res.send('All Sports Hub.......');
+})
+
+app.listen(port, () => {
+    console.log(`You Server Is Running on http://localhost:${port}`)
+})
