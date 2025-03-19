83f526364aad793ffab1e10ddc0552940be98b43
MdAsraful56 <work.mdasraful56@gmail.com>
2025-03-19 07:04:04 +0600

    mongodb connect

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000
 
@@ -8,10 +9,51 @@ const port = process.env.PORT || 5000
 app.use(cors());
 app.use(express.json());
 
+// AllSportManager
+// omaYQb7PmG0u4m4d
+
+const uri = "mongodb+srv://AllSportManager:omaYQb7PmG0u4m4d@cluster0.hvhc0.mongodb.net/?appName=Cluster0";
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+    });
+
+
 
 app.get('/', (req, res) => {
     res.send('All Sports Hub.......');
 })
+
+
+
+async function run() {
+try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+} finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+}
+}
+run().catch(console.dir);
+
+
+
+
+
+
+
+
+
 
 app.listen(port, () => {
     console.log(`You Server Is Running on http://localhost:${port}`)
